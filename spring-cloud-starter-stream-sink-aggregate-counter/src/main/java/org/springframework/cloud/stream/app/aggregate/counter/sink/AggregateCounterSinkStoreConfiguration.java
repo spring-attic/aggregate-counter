cc6f2a2ae2336fc366b090089ee6acdd840a6da4
Soby Chacko <schacko@pivotal.io>
2016-11-01 18:06:11 -0400

    Aggregate counter changes

---
+++ spring-cloud-starter-stream-sink-aggregate-counter/src/main/java/org/springframework/cloud/stream/app/aggregate/counter/sink/AggregateCounterSinkStoreConfiguration.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.cloud.stream.app.aggregate.counter.sink;
+
+import java.util.Collections;
+
+import org.springframework.analytics.metrics.AggregateCounterRepository;
+import org.springframework.analytics.metrics.redis.RedisAggregateCounterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.RedisConnectionFailureException;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.retry.RetryOperations;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+/**
+ * Configuration class for Redis based aggregate counter.
+ *
+ * @author Ilayaperumal Gopinathan
+ */
+@Configuration
+@EnableConfigurationProperties(AggregateCounterSinkProperties.class)
+public class AggregateCounterSinkStoreConfiguration {
+
+	@Autowired
+	private RedisConnectionFactory redisConnectionFactory;
+
+	@Autowired
+	private AggregateCounterSinkProperties config;
+
+	@Bean
+	public AggregateCounterRepository aggregateCounterRepository() {
+		return new RedisAggregateCounterRepository(redisConnectionFactory, retryOperations());
+	}
+
+	@Bean
+	public RetryOperations retryOperations() {
+		RetryTemplate retryTemplate = new RetryTemplate();
+		retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3,
+				Collections.<Class<? extends Throwable>, Boolean>singletonMap(RedisConnectionFailureException.class, true)));
+		ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
+		backOffPolicy.setInitialInterval(1000L);
+		backOffPolicy.setMaxInterval(1000L);
+		backOffPolicy.setMultiplier(2);
+		retryTemplate.setBackOffPolicy(backOffPolicy);
+		return retryTemplate;
+	}
+
+}
